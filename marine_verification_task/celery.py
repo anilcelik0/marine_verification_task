3ba0c634556d91b91078308f0d01fb33a69ced1e
anilcelik0 <anilcelik033@gmail.com>
2024-08-27 12:32:02 +0300

    last

--- marine_verification_task/celery.py
+++ marine_verification_task/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marine_verification_task.settings')
 
-app = Celery('marine_verification_task')
+app = Celery('marine_verification_task', broker='redis://redis:6379/0')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
